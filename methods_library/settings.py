6e83ec58756eb8015b5df4ccd9bdf48c07d61353
Florian Guldner <florian.guldner@googlemail.com>
2024-11-02 17:17:48 +0000

    create admin rights

--- methods_library/settings.py
+++ methods_library/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fl0w97-cip4methodslibra-36pag9fwswb.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
