3d1a6919ddec2f2376052217be5365d8fc8a230b
Florian Guldner <florian.guldner@googlemail.com>
2024-11-02 18:34:57 +0000

    model Method is added to models.py and database

--- view_methods/models.py
+++ view_methods/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Method(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="methods_library_methods"
+    )
+    purpose = models.CharField(max_length=255)
+    instructions = models.TextField()
+    material = models.TextField()
+    alt_atr = models.CharField(max_length=255)
+    prep_time = models.TimeField()
+    duration = models.DurationField()
+    group_size = models.IntegerField()
+    location = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
