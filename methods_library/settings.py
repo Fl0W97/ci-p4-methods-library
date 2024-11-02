5eba5724c85684d2b1a26dd76e58eb180af82f88
Florian Guldner <florian.guldner@googlemail.com>
2024-11-02 17:06:29 +0000

    set up postgreSQL database, add psycogp2, dj-database and adjust env.py and settings.py

--- methods_library/settings.py
+++ methods_library/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&*ny1u@#mr#1y(t4z=wjo2hd1o6bvbdfx_mzkpwr=p8xxx)e)'
+# SECRET_KEY = 'django-insecure-e&*ny1u@#mr#1y(t4z=wjo2hd1o6bvbdfx_mzkpwr=p8xxx)e)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-fl0w97-cip4methodslibra-36pag9fwswb.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -75,13 +80,16 @@ WSGI_APPLICATION = 'methods_library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
