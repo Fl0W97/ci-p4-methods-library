c72cee0eacc4928eb0e592d3211a84e037140c6c
Florian Guldner <florian.guldner@googlemail.com>
2024-11-09 15:13:32 +0000

    added method_page view, set DEBUG = False

--- methods_library/settings.py
+++ methods_library/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-fl0w97-cip4methodslibra-36pag9fwswb.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
