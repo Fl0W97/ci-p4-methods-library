056e22573b27de402a9c311d6bf864a5723f44f5
Florian Guldner <florian.guldner@googlemail.com>
2024-11-09 21:33:45 +0000

    add edit and delete btn for comments

--- view_methods/urls.py
+++ view_methods/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.MethodList.as_view(), name='home'),
     path('<slug:slug>/', views.method_page, name='method_page'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
--- view_methods/views.py
+++ view_methods/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Method
+from django.http import HttpResponseRedirect
+from .models import Method, Comment
 from .forms import CommentForm
 
 
@@ -52,3 +53,43 @@ def method_page(request, slug):
             "comment_form": comment_form,
         },
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Method.objects.filter(status=1)
+        method = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.method = method
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment is updated')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('method_page', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Method.objects.filter(status=1)
+    method = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted.')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments.')
+
+    return HttpResponseRedirect(reverse('method_page', args=[slug]))
