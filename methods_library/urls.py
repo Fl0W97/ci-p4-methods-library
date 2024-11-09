bf603f484746be6b8512bbc70ba148ce798ebff5
Florian Guldner <florian.guldner@googlemail.com>
2024-11-09 18:59:15 +0000

    customize admin panel by installing summernote app

--- methods_library/urls.py
+++ methods_library/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("view_methods.urls"), name="view_methods-urls"),
-    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
+    path("", include("view_methods.urls"), name="view_methods-urls"),
     # path('', index_views.index, name='index'),
 ]
--- view_methods/admin.py
+++ view_methods/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Method, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Method)
+class MethodAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'purpose')
+    search_fields = ['title','author']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('summary','instructions','material')
+
 
 # Register your models here.
-admin.site.register(Method)
 admin.site.register(Comment)
