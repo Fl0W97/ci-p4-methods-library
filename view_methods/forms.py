c85bd54b6178d65c4a36cfda5f1dacda6a33f4e2
Florian Guldner <florian.guldner@googlemail.com>
2024-11-16 19:16:30 +0000

    add styling to private_collection, take back split of method_form iinout fields in add_a_method

--- view_methods/forms.py
+++ view_methods/forms.py
@@ -1,4 +1,5 @@
 from .models import Comment, Method, About
+from django.utils.text import slugify
 from django import forms
 from django_summernote.widgets import SummernoteWidget
 
@@ -21,8 +22,7 @@ class MethodForm(forms.ModelForm):
             'location',
         )
 
-
-    # validation
+    # validation for group size
     def clean(self):
         cleaned_data = super().clean()
         group_size_min = cleaned_data.get('group_size_min')
@@ -33,14 +33,33 @@ class MethodForm(forms.ModelForm):
 
         return cleaned_data
 
-
+    # Save the form, ensuring the slug is set properly
     def save(self, commit=True):
         method = super().save(commit=False)
+
+        # If the slug is empty (i.e., it wasn't provided by the user), generate it from the title
+        if not method.slug and method.title:
+            method.slug = slugify(method.title)
+
+        # Ensure that the status is always set to 'Draft' when a new method is created
         if commit:
-            method.status = 0  # Ensure that the status is always set to 'Draft' when a new method is created
+            method.status = 0  
             method.save()
+
         return method
 
+    # Automatically generate the slug based on the title if not provided. 
+    def clean_slug(self): 
+        title = self.cleaned_data.get('title')
+        slug = self.cleaned_data.get('slug')
+        
+        # If slug is not provided, generate it from the title
+        if not slug and title:
+            slug = slugify(title)
+            self.cleaned_data['slug'] = slug  # Ensure the generated slug is included in cleaned data
+
+        
+        return slug
 
 class AboutForm(forms.ModelForm):
     class Meta:
